353210ce0f25acecac7c430fed0bb0fcae487aba
Jérémy Guyot <jeremyguy@orange.fr>
2021-02-15 12:07:54 +0100

    Ajout et configuration de flask-migrate

--- config/development.py
+++ config/development.py
@@ -6,4 +6,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 SECRET_KEY = os.getenv('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{BASE_DIR}/db.sqlite3'
+SQLALCHEMY_DATABASE_URI = (
+    os.getenv('DATABASE_URL') or f'sqlite:///{BASE_DIR}/db.sqlite3'
+)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- myapp/__init__.py
+++ myapp/__init__.py
@@ -4,10 +4,13 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 environment = os.getenv('FLASK_ENV')
 app.config.from_object(f'config.{environment}')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from . import views
+from . import models
